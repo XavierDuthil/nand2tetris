557aacf9c6e4611cbc3495a9b10df2828b3abb98
DUTHIL <@>
2013-07-07 21:35:52 +0200

    Mult fini

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -9,26 +9,23 @@
 // Put your code here.
 
 @1
-D=M
-
-@15
+D=M		// Charge l'opérande droite dans le registre D
+@15		// On stocke dans R15
 M=D     // $R15 = $R1
 
 @2
-M=0     // Flush $R2
+M=0     // Flush $R2 (résultat)
 
 (LOOP)
     @15
     D=M     // D = $R15
-
     @END
-    D;JLE   // if (D > 0) goto END
+    D;JLE   // if (D < 0) goto END (vérification de fin de boucle)
 
     @0
-    D=M     // D = $R0
-
+	D=M     // D = $R0 (on charge R0)
     @2
-    M=D+M   // $R2 += $R0
+    M=D+M   // $R2 += $R0 (résultat += opérande gauche)
 
     @15
     M=M-1   // $R15 -= 1
