d12a94ccbfbba1f2e798ea11994ca25ebc6a6b8a
DUTHIL <@>
2013-10-20 21:13:14 +0100

    Jump managed

--- 06/assembler.py
+++ 06/assembler.py
@@ -18,7 +18,7 @@ def readAsmProgram(asm_file):
 	for line in lines:
 		if not line.startswith("//") and len(line):
 			# asm_program contains the exploitable data
-			asm_program.append(line);
+			asm_program.append(line.strip());
 
 	return asm_program;
 
@@ -37,6 +37,7 @@ def assembleProgram(asm_program):
 			hackProgram.append(binaryInstruction);  # adds the instruction to the output list
 
 		regExResult2 = re.match('^(\w+)=(.+)$', line);
+		regExResult3 = re.match('^(.);(\w+)$', line);
 
 		# case: c-instruction
 		if regExResult2:
@@ -55,70 +56,50 @@ def assembleProgram(asm_program):
 				dest[2] = '1';
 			dest = "".join(dest);
 
-			# comp identification
-			if 'M' in extractedComp:
-				a = "1";
-			else:
-				a = "0";
-
-			# Replaces A or M by an X in extractedComp (same output instruction)
-			extractedComp = extractedComp.replace("A", "X");
-			extractedComp = extractedComp.replace("M", "X");
-
-			# All the cases
-			if extractedComp == "0":
-				c = "101010";
-			elif extractedComp == "1":
-				c = "111111";
-			elif extractedComp == "-1":
-				c = "111010";
-			elif extractedComp == "D":
-				c = "001100";
-			elif extractedComp == "X":
-				c = "110000";
-			elif extractedComp == "!D":
-				c = "001101";
-			elif extractedComp == "!X":
-				c = "110001";
-			elif extractedComp == "-D":
-				c = "001111";
-			elif extractedComp == "-X":
-				c = "110011";
-			elif extractedComp == "D+1":
-				c = "011111";
-			elif extractedComp == "X+1":
-				c = "110111";
-			elif extractedComp == "D-1":
-				c = "001110";
-			elif extractedComp == "X-1":
-				c = "110010";
-			elif extractedComp == "D+X":
-				c = "000010";
-			elif extractedComp == "D-X":
-				c = "010011";
-			elif extractedComp == "X-D":
-				c = "000111";
-			elif extractedComp == "D&X":
-				c = "000000";
-			elif extractedComp == "D|X":
-				c = "010101";
-			elif extractedComp == "X":
-				c = "110000";
-			elif extractedComp == "D":
-				c = "001100";
-			else:
-				raise Exception("Instruction %s non reconnue" % line)
-
-			comp = "{:s}{:s}".format(a,c);
+			# comp
+			comp = assembleComp(extractedComp);
 
 			# jump identification
 			jump = "000";
 
-			binaryInstruction = "111{:s}{:s}{:s}".format(comp, dest, jump);
+			binaryInstruction = assembleCInstruction(comp, dest, jump);
 
 			hackProgram.append(binaryInstruction);
 
-		if regExResult1 is None and regExResult2 is None: 
+		#case: JUMP instruction
+		if regExResult3:
+			extractedComp = regExResult3.group(1);
+			extractedJump = regExResult3.group(2);
+
+			# comp
+			comp = assembleComp(extractedComp);
+
+			# dest
+			dest = "000";
+
+			#jump identification
+			if extractedJump == "JGT":
+				jump = "001";
+			elif extractedJump == "JEQ":
+				jump = "010";
+			elif extractedJump == "JGE":
+				jump = "011";
+			elif extractedJump == "JLT":
+				jump = "100";
+			elif extractedJump == "JNE":
+				jump = "101";
+			elif extractedJump == "JLE":
+				jump = "110";
+			elif extractedJump == "JMP":
+				jump = "111";
+			else:
+				raise Exception("Jump \"%s\" non reconnu" % extractedJump)
+
+			binaryInstruction = assembleCInstruction(comp, dest, jump);
+
+			hackProgram.append(binaryInstruction);
+
+		if regExResult1 is None and regExResult2 is None and regExResult3 is None: 
 			hackProgram.append(line);
 
 	return hackProgram;
@@ -134,6 +115,67 @@ def writeHackProgram(hack_program, output_file):
 		with open(output_file, 'w') as f:
 			f.write(hack_program);
 
+def assembleComp(extractedComp):
+	# comp identification
+	if 'M' in extractedComp:
+		a = "1";
+	else:
+		a = "0";
+
+	# Replaces A or M by an X in extractedComp (same output instruction)
+	extractedComp = extractedComp.replace("A", "X");
+	extractedComp = extractedComp.replace("M", "X");
+
+	# All the cases
+	if extractedComp == "0":
+		c = "101010";
+	elif extractedComp == "1":
+		c = "111111";
+	elif extractedComp == "-1":
+		c = "111010";
+	elif extractedComp == "D":
+		c = "001100";
+	elif extractedComp == "X":
+		c = "110000";
+	elif extractedComp == "!D":
+		c = "001101";
+	elif extractedComp == "!X":
+		c = "110001";
+	elif extractedComp == "-D":
+		c = "001111";
+	elif extractedComp == "-X":
+		c = "110011";
+	elif extractedComp == "D+1":
+		c = "011111";
+	elif extractedComp == "X+1":
+		c = "110111";
+	elif extractedComp == "D-1":
+		c = "001110";
+	elif extractedComp == "X-1":
+		c = "110010";
+	elif extractedComp == "D+X":
+		c = "000010";
+	elif extractedComp == "D-X":
+		c = "010011";
+	elif extractedComp == "X-D":
+		c = "000111";
+	elif extractedComp == "D&X":
+		c = "000000";
+	elif extractedComp == "D|X":
+		c = "010101";
+	elif extractedComp == "X":
+		c = "110000";
+	elif extractedComp == "D":
+		c = "001100";
+	else:
+		raise Exception("Comp \"%s\" non reconnu" % extractedComp)
+
+	assembledComp = "{:s}{:s}".format(a,c);
+
+	return assembledComp;
+
+def assembleCInstruction(comp, dest, jump):
+	return "111{:s}{:s}{:s}".format(comp, dest, jump);
 
 if __name__ == "__main__":
 	args = readArguments();
